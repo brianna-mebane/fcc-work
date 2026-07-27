f62979a875e3d7489f962b3be54fbc688f60b37d
brianna <83041220+brianna-mebane@users.noreply.github.com>
2026-07-27 17:53:33 -0400

    Initialize JavaScript directory + trivia-bot.js Create new folder for JS cert + push trivia bot assignment to GitHub.

---
+++ 02-javascript/01-vars-and-strings/01-trivia-bot/trivia-bot.js
@@ -0,0 +1,29 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Alfrid";
+let botLocation = "Ghost Planet";
+let favoriteLanguage = "Python";
+
+const firstFact = "My name is " + botName + " and I live on " + botLocation + ".";
+
+console.log(firstFact);
+
+const secondFact = "My favorite programming language is " + favoriteLanguage + ".";
+
+console.log(secondFact);
+
+let codingFact = favoriteLanguage.concat(" got its name from the classic British comedy series 'Monty Python's Flying Circus'.");
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage.concat(" was originally developed as a summer vacation project by renowned programmer Guido van Rossum.");
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage.concat(" has been used in the development of apps from major companies like IBM, Netflix, and Facebook.");
+
+console.log(codingFact);
+
+const goodbyeMessage = "It was fun sharing these facts with you. Goodbye!" + "- " + botName + " from " + botLocation + ".";
+
+console.log(goodbyeMessage);
