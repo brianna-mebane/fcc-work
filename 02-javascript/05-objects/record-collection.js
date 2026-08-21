06278dafc1d6d815d76b06f9a7f5744292a9dfe0
brianna <83041220+brianna-mebane@users.noreply.github.com>
2026-08-20 23:31:53 -0400

    Commit completed record-collection.js code file Successfully filled in updateRecords function to complete lab tests.

--- 02-javascript/05-objects/record-collection.js
+++ 02-javascript/05-objects/record-collection.js
@@ -1,3 +1,14 @@
 function updateRecords(records, id, prop, value) {
-    
+    if (value.length == 0) {
+        delete records[id][prop];
+    } else if (prop != "tracks" && value.length !== 0) {
+        records[id][prop] = value;
+    } else if (prop == "tracks" && value.length !== 0 && records[id].hasOwnProperty("tracks") == false) {
+        records[id]["tracks"] = [];
+        records[id]["tracks"].push(value);
+    } else if (prop == "tracks" && value.length && records[id].hasOwnProperty("tracks")) {
+        records[id]["tracks"].push(value);
+    }
+
+    return records;
 }
