5f7903acc998e5de0da0a77123fdc839f7e55b4a
brianna <83041220+brianna-mebane@users.noreply.github.com>
2026-08-22 07:03:55 -0400

    Commit completed runSequence function to repo Successfully wrote runSequence code for traffic light task.

--- 02-javascript/06-loops/traffic-light-sequencer.js
+++ 02-javascript/06-loops/traffic-light-sequencer.js
@@ -29,3 +29,37 @@ const config4 = {
   fault: false,
   phases: []
 };
+
+function runSequence(config, cycles) {
+    const phases = config.phases;
+
+    if (config.fault) {
+        console.log("Faulted phase!");
+        return;
+    } else if (phases.length == 0) {
+        console.log("No phases found");
+        return;
+    } else {
+        let cycleCounter = 0;
+        while (cycleCounter < cycles) {
+            let i = 0;
+            while (i < phases.length) {
+                if (phases[i].duration <= 0) {
+                    console.log("Invalid phase detected");
+                } else {
+                    console.log(`Switching to ${phases[i].color} for ${phases[i].duration} s`);
+                }
+                i += 1;
+            }
+
+            cycleCounter += 1;
+        }
+    }
+}
+
+
+// runSequence(config1, 1);
+// runSequence(config1, 2);
+// runSequence(config2, 1);
+// runSequence(config3, 2);
+// runSequence(config4, 5);
