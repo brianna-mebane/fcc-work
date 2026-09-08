c9c8994aefb18071e3a676a0597d10613aca1f6d
brianna <83041220+brianna-mebane@users.noreply.github.com>
2026-09-07 20:48:51 -0400

    Commit parseShipment() in pantry-restocker.js Function passed in FCC editor, now going in repo.

---
+++ 02-javascript/06-loops/pantry-restocker.js
@@ -0,0 +1,42 @@
+const pantry = [
+  { sku: "A10", name: "Tomatoes", qty: 4, expires: "2027-01-01", zone: "fridge" },
+  { sku: "D43", name: "Pineapples", qty: 2, expires: "2020-01-01", zone: "general" }
+];
+
+const rawData = [
+  "A10|Tomatoes|5|2027-01-01",
+  "B21|Bananas|10|2027-01-01",
+  "C32|Eggs|3|2027-01-01|fridge",
+  "C32|Eggs|3|2027-01-01",
+  "D43|Pineapples|0|2027-01-01",
+  "E54|Peppers|-1|2027-01-01|fridge"
+];
+
+function parseShipment(rawData) {
+  const shipment = [];
+  const skus = [];
+
+  for (let datum of rawData) {
+    const parts = datum.split("|");
+    const sku = parts[0].trim();
+
+    if (!skus.includes(sku)) {
+      skus.push(sku);
+
+      const name = parts[1].trim();
+      const qty = parts[2].trim();
+      const expires = parts[3].trim();
+      const zone = (parts[4]) ? parts[4].trim() : "general";
+
+      shipment.push({
+        sku: sku,
+        name: name,
+        qty: parseInt(qty),
+        expires: expires,
+        zone: zone
+      });
+    }
+  }
+
+  return shipment;
+}
